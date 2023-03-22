9a7f470fec278f6f7643bae03e6f30c6f25f2041
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-03-22 15:48:13 +0100

    Specify provider versions

--- versions.tf
+++ versions.tf
@@ -1,3 +1,9 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 1.4.0"
+  required_providers {
+    random = ">= 3.4.3"
+    local = ">= 2.2.3"
+    tls = ">= 4.0.3"
+    aws = ">= 4.33.0"
+  }
 }
