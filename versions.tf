86de151cbc4f8ea224c352ad5dba20299461e55f
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-06-27 13:18:13 +0200

    Format versions file

--- versions.tf
+++ versions.tf
@@ -2,19 +2,19 @@ terraform {
   required_version = ">= 1.5.1"
   required_providers {
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "3.5.1"
     }
     local = {
-      source = "hashicorp/local"
+      source  = "hashicorp/local"
       version = "2.4.0"
     }
     tls = {
-      source = "hashicorp/tls"
+      source  = "hashicorp/tls"
       version = "4.0.4"
     }
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.5.0"
     }
   }
